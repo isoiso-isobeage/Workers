0ffdcde37b60024b45ffe31a3d3ed2bcfb66d3df
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-06-06 09:59:59 +0000

    [Add]各テーブル

---
+++ app/models/personnel.rb
@@ -0,0 +1,2 @@
+class Personnel < ApplicationRecord
+end
---
+++ db/migrate/20220606082757_create_sites.rb
@@ -0,0 +1,12 @@
+class CreateSites < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sites do |t|
+      t.integer :user_id, null: false
+      t.string :name, null: false
+      t.string :description
+
+
+      t.timestamps
+    end
+  end
+end

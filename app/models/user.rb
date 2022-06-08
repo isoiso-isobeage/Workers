56267cda25b22816995db9bf617f2e18ee66a291
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-06-08 12:30:23 +0000

    [Add]検索機能、フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,37 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  # フォロー関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
+  # フォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  # フォロー外す
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしているか
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  def get_full_name(user)
+    user.family_name + " " + user.name
+  end
+
 end

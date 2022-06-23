744c49d0193c66a9a61b2ee55201e30ec1c0cd02
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-06-23 09:53:23 +0000

    [fix]レスポンシブ　通知機能まで

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
-import "../stylesheets/application"
+
 import "@nathanvda/cocoon"
 
 Rails.start()

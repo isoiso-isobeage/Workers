d5c293247c43878d5fc999260b4eacb23810e795
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-06-23 12:45:25 +0000

    [fix]デプロイ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import "jquery";
 import "@fullcalendar/common/main.css"
 import "@fullcalendar/daygrid/main.css"
 import "@fullcalendar/list/main.css"
-import "@fullcalendar/timeglid/main.css"
+import "@fullcalendar/timegrid/main.css"
 import "@nathanvda/cocoon"
 
 Rails.start()

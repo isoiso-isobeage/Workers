17bfae95392986ef5bb79bfba0bf49979749a2ae
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-01 22:27:55 +0000

    [Add&Fix]中止通知、中止メール送信機能実装 日付表示修正 workテーブルにwork_statusカラムを追加

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -1,6 +1,7 @@
 class SiteMailer < ApplicationMailer
 
-  def send_site_mail(site, site_user)
+  # 予定作業の作成、更新時メール
+  def send_update_work_mail(site, site_user)
     @site = site
 
     @mail = SiteMailer.new()
@@ -11,9 +12,29 @@ class SiteMailer < ApplicationMailer
     )
   end
 
-  def self.send_notifications_to_site_user(site)
+  # 予定作業の作成、更新時メール送信
+  def self.send_update_work_to_site_user(site)
     site.users.each do |site_user|
-      SiteMailer.send_site_mail(site, site_user).deliver_now
+      SiteMailer.send_update_work_mail(site, site_user).deliver_now
+    end
+  end
+
+
+  def send_cansel_work_mail(site, work, site_user)
+    @site = site
+    @work = work
+
+    @mail = SiteMailer.new()
+    mail(
+      from: ENV['MAIL_ADDRESS'],
+      to:   site_user.email,
+      subject: '作業中止連絡'
+    )
+  end
+
+  def self.send_cansel_work_to_site_user(site, work)
+    site.users.each do |site_user|
+      SiteMailer.send_cansel_work_mail(site, work, site_user).deliver_now
     end
   end
 
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,11 +5,13 @@ class Notification < ApplicationRecord
   belongs_to :site, optional: true
   belongs_to :work, optional: true
   belongs_to :site_user, optional: true
+  # 送った通知
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
+  # 送られた通知
 	belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
 
 
-  enum action: { follow: 0, site_user: 1, create_work: 2, update_work: 3 }
+  enum action: { follow: 0, site_user: 1, create_work: 2, update_work: 3, cancel_work: 4 }
 
 
 end
